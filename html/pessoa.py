44d0d2dca414012b21b1cd171dbfdf84facd697f
PanicAThePython <nataliaweise@gmail.com>
2019-04-03 16:14:11 -0300

    Deletando pasta

--- html/pessoa.py
+++
@@ -1,8 +0,0 @@
-class Pessoa:
-
-    def __init__(self, nome, cpf):
-
-        self.nome = nome
-        self.cpf  = cpf
-
-lista = [ Pessoa("Natália", 6374678256856), Pessoa("Leandro", 7485585786786), Pessoa("Júlia", 7475672667370)]
